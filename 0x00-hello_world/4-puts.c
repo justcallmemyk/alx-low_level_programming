7c48ec32002474e81d03977a010bb11645710d78
Michael Waweru <itswaweru@gmail.com>
2022-06-02 11:34:23 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+// C program to show the use of puts
+#include <stdio.h>
+int main()
+{
+	puts("Geeksfor");
+	puts("Geeks");
+
+	getchar("Programming is like building a multilingual puzzle);
+	return 0;
+}
+
+
