b66a4ee79fa185419c3c44b6a2facbdc98dad266
Michael Waweru <itswaweru@gmail.com>
2022-06-02 18:01:33 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * Descriptio: main- prints sring stdout                                                                                                
+/**                                                        
+ * Descriptio: main- prints with proper grammar, but the outcome is a piece of artt                                                                                                
  * Return: 0 if success.                                                                                                                
- */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-        printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
-        return (0);                                                                                                                     
+ */                                                                              int main(void)                                                                                                                          
+{ printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
+ return (0);                                                                                                                     
 }
