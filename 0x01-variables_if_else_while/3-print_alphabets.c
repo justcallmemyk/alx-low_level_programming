1e6dfcecd93df0d4ed155cf1ffa865ebd8a83b4e
Michael Waweru <itswaweru@gmail.com>
2022-06-03 06:39:30 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,19 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <ctype.h>
 /**
- *  * main - all codes
- *   * Description:A program that prints alphabets in lowercase followed by a line
- *    *
- *     * Return: 0
- *      *
- *       */
-
+ * main - all codes
+ * Description:A program that prints alphabets in lowercase followed by a line
+ *
+ * Return: 0
+ *
+ */
 int main(void)
 {
-	        char alphabets;
-
-		        for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
-				        {
-						                putchar(alphabets);
-								        }
-			        putchar('\n');
-				        return (0);
+	char alphabets;
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	putchar('\n');
+	return (0);
 }
