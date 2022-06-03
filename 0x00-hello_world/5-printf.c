23486a7512bfd7655e65fa37570ad9c2fbea980d
Michael Waweru <itswaweru@gmail.com>
2022-06-02 17:55:11 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-/**
- * main - my entry point
- * Description: prints "with proper grammar, but the outcome is a piece of art"
- * Return: 0
-*/
-int main(void)
-{
-printf("\with proper grammar, but the outcome is a piece of art,");
-return (0);
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ * Descriptio: main- prints sring stdout                                                                                                
+ * Return: 0 if success.                                                                                                                
+ */                                                                                                                                     
+int main(void)                                                                                                                          
+{                                                                                                                                       
+        printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
+        return (0);                                                                                                                     
 }
